738dba783c371160364ca584dbc07483b740c3af
Rosie Le Faive <lefaive@gmail.com>
2017-11-21 15:26:21 -0400

    Add useful modules and xdebug.

--- scripts/post.sh
+++ scripts/post.sh
@@ -12,3 +12,23 @@ chmod -R 775 /var/www/drupal/sites/default/files
 drush --root=/var/www/drupal role-add-perm "anonymous user" "view fedora repository objects"
 drush --root=/var/www/drupal role-add-perm "authenticated user" "view fedora repository objects"
 drush --root=/var/www/drupal cc all
+
+# Add useful modules
+drush --root=/var/www/drupal -v -y en admin_menu module_filter
+drush --root=/var/www/drupal -v -y pm-disable toolbar
+
+# Add xdebug
+
+sudo apt-get install php5-xdebug
+
+
+cat <<'EOT' >> /etc/php5/apache2/php.ini
+
+echo 'zend_extension=/usr/lib/php5/20121212/xdebug.so'
+echo 'xdebug.remote_enable=on'
+echo 'xdebug.remote_handler=dbgp'
+echo 'xdebug.remote_host=localhost:8000'
+echo 'xdebug.remote_port=9000'
+echo 'xdebug.remote_connect_back=1'
+EOT
+sudo service apache2 restart
